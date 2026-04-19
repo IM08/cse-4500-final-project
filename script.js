a9332d015ebe81420d35c5c2ee297b2470eec993
Isaac <isaac.munoz0808@gmail.com>
2026-04-18 17:36:07 -0700

    Added basic newsletter modal

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Use this for global javascript
+
+function showModal(modalName) {
+    document.getElementById(modalName).style.display = "block";
+}
+
+function hideModal(modalName) {
+    document.getElementById(modalName).style.display = "none";
+}
+
+// Hides modal if user clicked outside its borders
+function modalClicked(event) {
+    if (event.target.className == "modal") {
+        // User clicked modal background, not content
+        let modalName = event.target.id;
+        hideModal(modalName);
+    }
+}
